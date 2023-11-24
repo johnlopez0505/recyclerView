0b9dccc414a39dddb86f6278b064445b3cf70af4
john-lopez <lopezcon1@hotmail.com>
2023-11-24 22:40:54 +0100

    version 0.0.1

---
+++ app/src/main/java/adapter/ViewHHotel.kt
@@ -0,0 +1,26 @@
+package adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.john.recicleview.databinding.ItemHotelBinding
+import models.Hotel
+
+class ViewHHotel (view: View) : RecyclerView.ViewHolder (view){
+    lateinit var binding: ItemHotelBinding
+    init {
+        binding = ItemHotelBinding.bind(view)
+    }
+    //método que se encarga de mapear los item por propiedad del modelo.
+    fun renderize(hotel : Hotel){
+        binding.txtviewName.setText(hotel. name)
+        binding.txtviewCity.setText(hotel. city)
+        binding.txtviewProvince.setText(hotel. province)
+        binding.txtviewPhone.setText(hotel. phone)
+        Glide
+            .with( itemView.context)
+            .load(hotel. image)
+            .centerCrop()
+            .into( binding.ivHotel)
+    }
+}
